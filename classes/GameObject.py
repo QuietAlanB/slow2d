a151b9f9676f976be5ff133a8f09e555b0cd55b3
QuietAlanB <alan.belov.2008@gmail.com>
2022-02-26 13:32:31 +0400

    Update GameObject.py

--- classes/GameObject.py
+++ classes/GameObject.py
@@ -4,6 +4,11 @@ class GameObject:
     def __init__(self, pos):
         self.lastPos = pos
         self.pos = pos
+        self.components = []
+
+        # TODO:
+        # make components and give each an update function
+        # and just update them
 
     def lastPosUpdate(self):
         self.lastPos = Vector2(self.pos.x, self.pos.y)
